212387d6303813863f8a62f09c71dbff9fc67f45
enreap DevOps Team <184435543+enreap@users.noreply.github.com>
2025-12-16 15:09:30 +0530

    Create proc_create_users.sql

---
+++ sql/proc_create_users.sql
@@ -0,0 +1,17 @@
+-- Create users table if not exists
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50),
+    email VARCHAR(100)
+);
+
+-- Stored procedure to add a new user
+DELIMITER $$
+
+DROP PROCEDURE IF EXISTS add_user$$
+CREATE PROCEDURE add_user(IN p_username VARCHAR(50), IN p_email VARCHAR(100))
+BEGIN
+    INSERT INTO users(username, email) VALUES (p_username, p_email);
+END$$
+
+DELIMITER ;
