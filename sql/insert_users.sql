e985e38a75b08ee705b34451d9e0ee0b0d83dfc1
enreap DevOps Team <184435543+enreap@users.noreply.github.com>
2025-12-30 22:28:58 +0530

    Update insert_users.sql

--- sql/insert_users.sql
+++ sql/insert_users.sql
@@ -1,7 +1,9 @@
-INSERT INTO users (username, email, company, domain, team) VALUES ('babu', 'babu@enreap.com', 'enreap', 'devops cloud', 'devops');
-INSERT INTO users (username, email, company, domain, team) VALUES ('avinash', 'avinash@enreap.com', 'enreap', 'devops cloud', 'devops');
-INSERT INTO users (username, email, company, domain, team) VALUES ('ketki', 'ketki@enreap.com', 'enreap', 'devops cloud', 'devops');
-INSERT INTO users (username, email, company, domain, team) VALUES ('suhas', 'suhas@enreap.com', 'enreap', 'devops cloud', 'devops');
-INSERT INTO users (username, email, company, domain, team) VALUES ('sangram', 'sangram@enreap.com', 'enreap', 'devops cloud', 'devops');
-INSERT INTO users (username, email, company, domain, team) VALUES ('nithis', 'nithis@enreap.com', 'enreap', 'devops cloud', 'devops');
+INSERT INTO users (username, email) VALUES
+('babu', 'babu@enreap.com'),
+('avinash', 'avinash@enreap.com'),
+('ketki', 'ketki@enreap.com');
 
+INSERT INTO orders (status) VALUES
+('NEW'),
+('PROCESSING'),
+('DONE');
