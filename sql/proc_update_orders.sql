a09c1cccf80a6fb43062ce03f7a42f6821ed8342
enreap DevOps Team <184435543+enreap@users.noreply.github.com>
2025-12-30 18:38:35 +0530

    Update proc_update_orders.sql

--- sql/proc_update_orders.sql
+++ sql/proc_update_orders.sql
@@ -5,10 +5,8 @@ CREATE TABLE IF NOT EXISTS orders (
 );
 
 -- Stored procedure to update order status
-DELIMITER $$
-DROP PROCEDURE IF EXISTS update_order_status$$
+DROP PROCEDURE IF EXISTS update_order_status;
 CREATE PROCEDURE update_order_status(IN p_order_id INT, IN p_status VARCHAR(20))
 BEGIN
     UPDATE orders SET status = p_status WHERE id = p_order_id;
-END$$
-DELIMITER ;
+END;
