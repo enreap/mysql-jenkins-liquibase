98104d0a2ca0a06c6281f03a159b01ff93f12eda
enreap DevOps Team <184435543+enreap@users.noreply.github.com>
2025-12-30 19:24:54 +0530

    Create insert_users.sql

---
+++ sql/insert_users.sql
@@ -0,0 +1,12 @@
+INSERT INTO users (username, email, company, domain, team) VALUES
+('babu', 'babu@enreap.com', 'enreap', 'devops cloud', 'devops');
+INSERT INTO users (username, email, company, domain, team) VALUES
+('avinash', 'avinash@enreap.com', 'enreap', 'devops cloud', 'devops');
+INSERT INTO users (username, email, company, domain, team) VALUES
+('ketki', 'ketki@enreap.com', 'enreap', 'devops cloud', 'devops');
+INSERT INTO users (username, email, company, domain, team) VALUES
+('suhas', 'suhas@enreap.com', 'enreap', 'devops cloud', 'devops');
+INSERT INTO users (username, email, company, domain, team) VALUES
+('sangram', 'sangram@enreap.com', 'enreap', 'devops cloud', 'devops');
+INSERT INTO users (username, email, company, domain, team) VALUES
+('nithis', 'nithis@enreap.com', 'enreap', 'devops cloud', 'devops');
