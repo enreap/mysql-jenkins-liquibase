37e4bf8df6a0e3ebb50df1e505dffe1e1857bd86
enreap DevOps Team <184435543+enreap@users.noreply.github.com>
2026-01-02 14:43:44 +0530

    Update proc_create_users.sql
    added create

--- sql/proc_create_users.sql
+++ sql/proc_create_users.sql
@@ -1,5 +1,14 @@
-DROP PROCEDURE IF EXISTS add_user;
-CREATE PROCEDURE add_user(IN uname VARCHAR(50), IN mail VARCHAR(100))
+CREATE PROCEDURE add_user(
+    IN uname VARCHAR(50),
+    IN mail VARCHAR(100)
+)
 BEGIN
-    INSERT INTO users(username, email) VALUES (uname, mail);
-END;
+    INSERT INTO users(username, email)
+    VALUES (uname, mail);
+END
+
+-- DROP PROCEDURE IF EXISTS add_user;
+-- CREATE PROCEDURE add_user(IN uname VARCHAR(50), IN mail VARCHAR(100))
+-- BEGIN
+--     INSERT INTO users(username, email) VALUES (uname, mail);
+-- END;
